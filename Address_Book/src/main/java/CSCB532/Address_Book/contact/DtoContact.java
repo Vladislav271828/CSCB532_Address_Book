b64e677a29efc9c0cb91784770a79f843eaed20d
Vladislav Dimov <vladislavdimovvv@gmail.com>
2023-11-08 17:19:32 +0200

    Created: ContactRepository ContactService ContactController DtoContact

---
+++ Address_Book/src/main/java/CSCB532/Address_Book/contact/DtoContact.java
@@ -0,0 +1,24 @@
+package CSCB532.Address_Book.contact;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DtoContact {
+
+    private int importance;
+    private String name;
+    private String lastName;
+    private String phoneNumber;
+    private String nameOfCompany;
+    private String address;
+    private String email;
+    private String fax;
+    private String mobileNumber;
+    private String comment;
+}
