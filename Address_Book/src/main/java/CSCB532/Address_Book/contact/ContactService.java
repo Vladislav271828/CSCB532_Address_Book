2302aca1db8b185e63d304a9cda641367717e4cf
Vladislav Dimov <vladislavdimovvv@gmail.com>
2023-11-08 17:34:47 +0200

    used ModelMapper for createContact() to avoid mucho texto

--- Address_Book/src/main/java/CSCB532/Address_Book/contact/ContactService.java
+++ Address_Book/src/main/java/CSCB532/Address_Book/contact/ContactService.java
@@ -3,6 +3,7 @@ package CSCB532.Address_Book.contact;
 import CSCB532.Address_Book.auth.AuthenticationService;
 import CSCB532.Address_Book.exception.ApiException;
 import CSCB532.Address_Book.user.User;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,34 +20,35 @@ public class ContactService {
     public DtoContact createContact(DtoContact dtoContact) throws ApiException {
         if (dtoContact.toString().isEmpty()) return null;
         User user = authenticationService.getCurrentlyLoggedUser();
-        //to shorten with objectmapper
-        Contact contact = new Contact();
+
+        ModelMapper modelMapper = new ModelMapper();
+        Contact contact = modelMapper.map(dtoContact, Contact.class);
         contact.setUser(user);
-        contact.setImportance(dtoContact.getImportance());
-        contact.setName(dtoContact.getName());
-        contact.setLastName(dtoContact.getLastName());
-        contact.setPhoneNumber(dtoContact.getPhoneNumber());
-        contact.setNameOfCompany(dtoContact.getNameOfCompany());
-        contact.setAddress(dtoContact.getAddress());
-        contact.setEmail(dtoContact.getEmail());
-        contact.setFax(dtoContact.getFax());
-        contact.setMobileNumber(dtoContact.getMobileNumber());
-        contact.setComment(dtoContact.getComment());
+//        contact.setImportance(dtoContact.getImportance());
+//        contact.setName(dtoContact.getName());
+//        contact.setLastName(dtoContact.getLastName());
+//        contact.setPhoneNumber(dtoContact.getPhoneNumber());
+//        contact.setNameOfCompany(dtoContact.getNameOfCompany());
+//        contact.setAddress(dtoContact.getAddress());
+//        contact.setEmail(dtoContact.getEmail());
+//        contact.setFax(dtoContact.getFax());
+//        contact.setMobileNumber(dtoContact.g  etMobileNumber());
+//        contact.setComment(dtoContact.getComment());
         contactRepository.save(contact);
         contact = contactRepository.findTopByUserIdOrderByContactIdDesc(user.getId()).get();
 
-
-        dtoContact.setImportance(contact.getImportance());
-        dtoContact.setAddress(contact.getAddress());
-        dtoContact.setName(contact.getName());
-        dtoContact.setLastName(contact.getLastName());
-        dtoContact.setPhoneNumber(contact.getPhoneNumber());
-        dtoContact.setNameOfCompany(contact.getNameOfCompany());
-        dtoContact.setAddress(contact.getAddress());
-        dtoContact.setEmail(contact.getEmail());
-        dtoContact.setFax(contact.getFax());
-        dtoContact.setMobileNumber(contact.getMobileNumber());
-        dtoContact.setComment(contact.getComment());
+        dtoContact = modelMapper.map(contact, DtoContact.class);
+//        dtoContact.setImportance(contact.getImportance());
+//        dtoContact.setAddress(contact.getAddress());
+//        dtoContact.setName(contact.getName());
+//        dtoContact.setLastName(contact.getLastName());
+//        dtoContact.setPhoneNumber(contact.getPhoneNumber());
+//        dtoContact.setNameOfCompany(contact.getNameOfCompany());
+//        dtoContact.setAddress(contact.getAddress());
+//        dtoContact.setEmail(contact.getEmail());
+//        dtoContact.setFax(contact.getFax());
+//        dtoContact.setMobileNumber(contact.getMobileNumber());
+//        dtoContact.setComment(contact.getComment());
 
         return dtoContact;
 
